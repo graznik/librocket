f62e1d01b193dbe116ead15d20f790e2c05d47b9
Michael Hornung <mhornung.linux@gmail.com>
2015-09-28 22:25:58 +0200

    Add new file librocket.h and move things over
    * Move Winbond rocket launcher defines to librocket.h.
    * Move struct rocket_launcher declaration to librocket.h.
    * Add function prototypes to librocket.h.

---
+++ librocket.h
@@ -0,0 +1,30 @@
+#include <libusb.h>
+
+/* Winbond PE-5858-919 */
+#define WINBOND_VENDOR_ID 0x0416  /* Winbond */
+#define WINBOND_DEVICE_ID 0x9391  /* PE-5858-919 */
+#define WINBOND_HOLD       0x60
+#define WINBOND_DOWN       0x01
+#define WINBOND_UP         0x02
+#define WINBOND_RIGHT      0x04
+#define WINBOND_LEFT       0x08
+#define WINBOND_FIRE       0x1f
+#define WINBOND_LOWER_END  0xe1
+
+struct rocket_launcher {
+	unsigned char last_cmd;
+	/* Direction commands (host -> device) */
+	unsigned char cmd_hold;
+	unsigned char cmd_up;
+	unsigned char cmd_down;
+	unsigned char cmd_left;
+	unsigned char cmd_right;
+	unsigned char cmd_fire;
+	int (*control)(struct rocket_launcher *self, unsigned char dir);
+	/* USB device handle */
+	libusb_device_handle *handle;
+};
+
+static int __winbond_control(struct rocket_launcher *self, unsigned char dir);
+void init_winbond(struct rocket_launcher *winb, libusb_device_handle *handle);
+libusb_device *get_device(libusb_device **devs, int cnt);
