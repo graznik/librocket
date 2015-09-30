13bb95a431bd6e69fa07023fa5cd2ddde5ee38f6
Michael Hornung <mhornung.linux@gmail.com>
2015-09-30 21:05:17 +0200

    examples: basic: Remove obsolete USB initialization
    * Remove obsolete code for USB intialization.
    * Add calls to librocket wrapper functions init_launcher() and
      exit_launcher().
    * Return EXIT_SUCCESS on success ;-)

--- examples/basic/rocket_launcher.c
+++ examples/basic/rocket_launcher.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <ncurses.h>
-#include <libusb.h>
 #include "../../librocket.h"
 
 #define UP    65
@@ -11,87 +10,16 @@
 #define LEFT  68
 #define FIRE  '\n'
 
-libusb_device *get_device(libusb_device **devs, int cnt)
-{
-	int err, i;
-	libusb_device *dev;
-	struct libusb_device_descriptor dev_desc;
-
-	for (i = 0; i < cnt; i++) {
-		dev = devs[i];
-		err = libusb_get_device_descriptor(dev, &dev_desc);
-		if (err) {
-			fprintf(stderr, "%s\n", libusb_error_name(err));
-			return NULL;
-		}
-
-		if ((dev_desc.idVendor == WINBOND_VENDOR_ID) &&
-		    (dev_desc.idProduct == WINBOND_DEVICE_ID))
-			return dev;
-		else
-			fprintf(stdout, "Ignoring 0x%04x 0x%04x\n",
-				dev_desc.idVendor,
-				dev_desc.idProduct);
-	}
-	return NULL;
-}
-
 int main(int argc, char *argv[])
 {
-	libusb_context *ctx;
-	libusb_device **devs;
-	libusb_device *dev;
-	libusb_device_handle *handle;
 	struct rocket_launcher *rl = NULL;
-	ssize_t cnt;
-	uint8_t bus, addr;
-	int err = 0;
-	int retval;
 	unsigned char cmd = 0, next_cmd = 0;
-
-	err = libusb_init(&ctx);
-	if (err) {
-		fprintf(stderr, "%s\n", libusb_error_name(err));
-		return EXIT_FAILURE;
-	}
-
-	cnt = libusb_get_device_list(ctx, &devs);
-
-	dev = get_device(devs, cnt);
-	if (!dev) {
-		fprintf(stderr, "Error: Unable to detect device\n");
-		retval = EXIT_FAILURE;
-		goto err_free;
-	}
-
-	bus = libusb_get_bus_number(dev);
-	addr = libusb_get_device_address(dev);
-
-	fprintf(stdout, "Bus %03d Device %03d\n", bus, addr);
-
-	err = libusb_open(dev, &handle);
-	if (err) {
-		fprintf(stderr, "%s\n", libusb_error_name(err));
-		retval = EXIT_FAILURE;
-		goto err_free;
-	}
-
-	err = libusb_kernel_driver_active(handle, 0);
-	if (err) {
-		err = libusb_detach_kernel_driver(handle, 0);
-		if (err) {
-			fprintf(stderr, "%s\n", libusb_error_name(err));
-			retval = EXIT_FAILURE;
-			goto err_close;
-		}
-	}
 
 	initscr();
 	noecho();
 
 	rl = malloc(sizeof(struct rocket_launcher));
-	/* FIXME: Only Winbond device supported at the moment */
-	init_winbond(rl, handle);
+	init_launcher(rl);
 
 	while (1) {
 		next_cmd = (unsigned char)getch();
@@ -128,15 +56,8 @@ int main(int argc, char *argv[])
 		rl->control(rl, cmd);
 	}
 
-	retval = EXIT_SUCCESS;
-
- err_close:
-	libusb_close(handle);
- err_free:
-	libusb_free_device_list(devs, 1);
-
-	libusb_exit(ctx);
+	exit_launcher(rl);
 	free(rl);
 
-	return retval;
+	return EXIT_SUCCESS;
 }
